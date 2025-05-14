8345f40c754b456612a699144aba415d644bb97b
Lucas Bachur <lucasbachur11@gmail.com>
2025-05-14 02:44:22 -0300

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import './App.css'
+import Kpopdle from './components/Kpopdle'
+
+const idols = [
+  { id: 1, name: 'Nayeon', group: 'Twice', nationality: 'Korean', age: 27 },
+  { id: 2, name: 'Jeongyeon', group: 'Twice', nationality: 'Korean', age: 26 },
+  { id: 3, name: 'Momo', group: 'Twice', nationality: 'Japanese', age: 26 },
+  { id: 4, name: 'Sana', group: 'Twice', nationality: 'Japanese', age: 26 },
+  { id: 5, name: 'Jihyo', group: 'Twice', nationality: 'Korean', age: 26 },
+  { id: 6, name: 'Mina', group: 'Twice', nationality: 'Japanese', age: 26 },
+  { id: 7, name: 'Dahyun', group: 'Twice', nationality: 'Korean', age: 25 },
+  { id: 8, name: 'Chaeyoung', group: 'Twice', nationality: 'Korean', age: 24 },
+  { id: 9, name: 'Tzuyu', group: 'Twice', nationality: 'Taiwanese', age: 24 },
+  { id: 10, name: 'Karina', group: 'Aespa', nationality: 'Korean', age: 23 },
+  { id: 11, name: 'Giselle', group: 'Aespa', nationality: 'Japanese', age: 23 },
+  { id: 12, name: 'Winter', group: 'Aespa', nationality: 'Korean', age: 22 },
+  { id: 13, name: 'Ningning', group: 'Aespa', nationality: 'Chinese', age: 21 }
+]
+
+const answer = { id: 4, name: 'Sana', group: 'Twice', nationality: 'Japanese', age: 26 };
+
+function App() {
+  return (
+    <>
+      <Kpopdle idolData={idols} answer={answer} />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Kpopdle.jsx
@@ -0,0 +1,61 @@
+import './Kpopdle.css';
+import { useState } from 'react';
+
+function GuessField({value, answerValue}){
+    return(
+        <div className={`guess ${value === answerValue ? 'correct' : 'incorrect'}`}>{value}</div>
+    );
+}
+
+function Guess({ guess, answer }) {
+    const { name, group, age, nationality } = guess;
+    return (
+        <div className='guess-container'>
+            <GuessField value={name} answerValue={answer.name} />
+            <GuessField value={group} answerValue={answer.group} />
+            <GuessField value={age} answerValue={answer.age} />
+            <GuessField value={nationality} answerValue={answer.nationality} />
+        </div>
+    );
+}
+
+function GuessList({ guesses, answer }) {
+    return (
+        <div>
+            {guesses.map((guess) => {
+                return <Guess guess={guess} answer={answer}/>;
+            })}
+        </div>
+    );
+}
+
+function Kpopdle({ idolData, answer }) {
+
+    const [guesses, setGuesses] = useState([]);
+    const [victory, setVictory] = useState(false);
+
+    const findIdol = (name) => {
+        const idol = idolData.find(idol => idol.name.toLowerCase() === name.toLowerCase());
+        return idol ? idol : null;
+    }
+
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            const inputValue = event.target.value;
+            const guessedIdol = findIdol(inputValue);
+            if (guessedIdol.id === answer.id) {
+                setVictory(true);
+            }
+            setGuesses([...guesses, guessedIdol]);
+            event.target.value = ''; // Clear the input field after pressing Enter
+        }
+    }
+
+    return (
+        <div>
+            <input type="text" onKeyDown={handleKeyDown} placeholder="Type your guess" disabled={victory}/>
+            <GuessList guesses={guesses} answer={answer}/>
+        </div>
+    );
+}
+export default Kpopdle;
